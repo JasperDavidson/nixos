b4801cbfe6e3355838d2f36ea22b3d30667861bb
JasperDavidson <jasperdavdsn@gmail.com>
2025-02-06 18:42:38 -0700

    Small update before reinstall

---
+++ nix_files/flake.nix
@@ -0,0 +1,45 @@
+{
+
+   description = "Config flake";
+
+   inputs = {
+      nixpkgs = { url = "github:NixOS/nixpkgs/nixos-unstable"; };
+
+      # Home-manager flake
+      home-manager = { url = "github:nix-community/home-manager/master"; inputs.nixpkgs.follows = "nixpkgs"; };
+
+      # Hyprland flake
+      hyprland = { url = "github:hyprwm/Hyprland"; };
+
+      # Hyprland plugins
+      hyprland-plugins = {
+         url = "github:hyprwm/hyprland-plugins";
+         inputs.hyprland.follows = "hyprland";
+      };
+
+      # Helix flake
+      # helix = { url = "github:helix-editor/helix"; };
+   };
+
+   outputs =  {self, nixpkgs, home-manager, ... }@inputs:
+   let
+      lib = nixpkgs.lib;
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+   in {
+      nixosConfigurations = {
+         orthocenter = lib.nixosSystem {
+            specialArgs = { inherit inputs; inherit system; };
+            modules = [ ./configuration.nix ];
+         };
+      };
+
+      homeConfigurations = {
+         jasperd = home-manager.lib.homeManagerConfiguration {
+            inherit pkgs;
+            extraSpecialArgs = { inherit inputs; };
+            modules = [ ./home.nix ];
+         };
+      };
+   };
+}
